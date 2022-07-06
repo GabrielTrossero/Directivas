c3ecc12a80b5e26bc15be257a9e481b0d8381b04
Gabriel Trossero <gabi_trossero@hotmail.com>
2022-07-06 17:06:52 -0300

    modificacion directiva personalizada

--- src/app/directivas/subrayado.directive.ts
+++ src/app/directivas/subrayado.directive.ts
@@ -1,13 +1,16 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appSubrayado]'
 })
 export class SubrayadoDirective {
 
-  constructor(elem: ElementRef) { 
+  constructor(elem: ElementRef, renderer: Renderer2) { 
     //Al elemento sobre el cual le estoy aplicando la directiva, le modifico el style del texto
-    elem.nativeElement.style.textDecoration = 'underline';
+    //elem.nativeElement.style.textDecoration = 'underline';
+    
+    //Esto hace lo mismo que el de arriba pero de una forma idependiente de la plataforma que utilicemos 
+    renderer.setStyle(elem.nativeElement, 'text-decoration', 'underline');
   }
 
 }
