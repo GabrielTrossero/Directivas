294f6ce07f502ed485fc21a0080dceb4ce80b370
Gabriel Trossero <gabi_trossero@hotmail.com>
2022-07-06 17:13:03 -0300

    @HostListener - directivas personalizadas

--- src/app/directivas/subrayado.directive.ts
+++ src/app/directivas/subrayado.directive.ts
@@ -1,16 +1,25 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appSubrayado]'
 })
 export class SubrayadoDirective {
 
-  constructor(elem: ElementRef, renderer: Renderer2) { 
+  constructor(private elem: ElementRef, private renderer: Renderer2) { 
     //Al elemento sobre el cual le estoy aplicando la directiva, le modifico el style del texto
     //elem.nativeElement.style.textDecoration = 'underline';
     
     //Esto hace lo mismo que el de arriba pero de una forma idependiente de la plataforma que utilicemos 
-    renderer.setStyle(elem.nativeElement, 'text-decoration', 'underline');
+    this.renderer.setStyle(this.elem.nativeElement, 'text-decoration', 'underline');
+    this.renderer.setStyle(this.elem.nativeElement, 'color', 'indigo');
+  }
+
+  @HostListener('mouseover') onHover() {
+    this.renderer.setStyle(this.elem.nativeElement, 'color', 'green');
+  }
+  
+  @HostListener('mouseout') onMouseOut() {
+    this.renderer.setStyle(this.elem.nativeElement, 'color', 'indigo');
   }
 
 }
